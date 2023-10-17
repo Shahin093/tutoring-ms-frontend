5a98a4f60a6491848f9a2f7adb416d7b6920d8bf
Shidul Islam <sishahin093@gmail.com>
2023-10-17 09:50:41 +0600

    booking API done

---
+++ src/redux/api/bookingApi.ts
@@ -0,0 +1,65 @@
+import { baseApi } from "./baseApi";
+import { tagTypes } from "../tag-types";
+import { IMeta, IService } from "@/types";
+
+const BOOKING_URL = "/bookings";
+
+export const bookingApi = baseApi.injectEndpoints({
+  endpoints: (build) => ({
+    addBookingData: build.mutation({
+      query: (data) => ({
+        url: `${BOOKING_URL}/create-booking`,
+        method: "POST",
+        data,
+      }),
+      invalidatesTags: [tagTypes.booking],
+    }),
+
+    bookings: build.query({
+      query: (arg: Record<string, any>) => {
+        return {
+          url: BOOKING_URL,
+          method: "GET",
+          params: arg,
+        };
+      },
+      transformResponse: (response: IService[], meta: IMeta) => {
+        return {
+          services: response,
+          meta,
+        };
+      },
+      providesTags: [tagTypes.booking],
+    }),
+
+    booking: build.query({
+      query: (id) => {
+        return {
+          url: `${BOOKING_URL}/${id}`,
+          method: "GET",
+        };
+      },
+
+      providesTags: [tagTypes.booking],
+    }),
+
+    updateBooking: build.mutation({
+      query: (data) => ({
+        url: `${BOOKING_URL}/${data.id}`,
+        method: "PATCH",
+        data,
+      }),
+      invalidatesTags: [tagTypes.booking],
+    }),
+
+    deleteBooking: build.mutation({
+      query: (id) => ({
+        url: `${BOOKING_URL}/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: [tagTypes.booking],
+    }),
+  }),
+});
+
+export const {} = bookingApi;
--- src/redux/tag-types.ts
+++ src/redux/tag-types.ts
@@ -2,6 +2,12 @@ export enum tagTypes {
   user = "user",
   profile = "profile",
   service = "service",
+  booking = "booking",
 }
 
-export const tagTypesList = [tagTypes.user, tagTypes.profile, tagTypes.service];
+export const tagTypesList = [
+  tagTypes.user,
+  tagTypes.profile,
+  tagTypes.service,
+  tagTypes.booking,
+];
